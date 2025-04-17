824a69fc0fe47cc1b84cd274b4306852fd540ef5
Siu Sab <san24022005@example.com>
2025-04-17 21:47:28 +0700

    Cap nhat

--- widget/header.php
+++ widget/header.php
@@ -1,3 +1,35 @@
 <div id="header">
+    <div class="logo">
+        <a href="index.php"></a>
+    </div>
 
+    <ul class="nav">
+        <li>
+            <a href="">
+                Áo
+                <i class="down-icon ti-angle-down"></i>
+            </a>
+            <ul class="sub-nav">
+                <li><a href="">Áo polo dài tay</a></li>
+                <li><a href="">Áo thun dài tay</a></li>
+                <li><a href="">Áo khoác</a></li>
+                <li><a href="">Áo sơ mi</a></li>
+                <li><a href="">Áo Tshirt</a></li>
+            </ul>
+        </li>
+        <li>
+            <a href="">
+                Quần
+                <i class="down-icon ti-angle-down"></i>
+            </a>
+            <ul class="sub-nav">
+                <li><a href="">Quần âu</a></li>
+                <li><a href="">Quần short</a></li>
+                <li><a href="">Quần jean</a></li>
+            </ul>
+        </li>
+        <li><a href="">Đồ bộ</a></li>
+        <li><a href="">Đồng phục</a></li>
+        <li><a href="">Tây phục</a></li>
+    </ul>
 </div>
