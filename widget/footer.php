fee51c1ad5b979df25e50df4600cb09abd7bda07
Siu Sab <san24022005@example.com>
2025-04-22 21:13:13 +0700

    update footer

--- widget/footer.php
+++ widget/footer.php
@@ -2,34 +2,95 @@
     <div id="top-footer">
         <ul>
             <li>
+            <div class="footer-box">
                 <div class="logo">
                     <a href="index.php"></a>
                 </div>
-                <div class="footer-box">
+               
                     <ul>
                         <li>Thương hiệu thời trang nam cao cấp, 
                             hướng đến xây dựng phong cách thời trang lịch lãm, 
                             sang trọng cho phái mạnh. Sản phẩm mẫu mã đa dạng, 
                             hệ thống phân phối bán buôn, bán lẻ, đại lý, 
                             cửa hàng và TTTM trên toàn quốc.</li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
+                        <li>
+                            <i class="icon-footer ti-location-pin"></i>
+                            <a href="">Địa chỉ: Quy Nhơn, Bình định</a>
+                        </li>
+                        <li>
+                            <i class="icon-footer ti-mobile"></i>
+                            <a href="">123456789</a>
+                        </li>
+                        <li>
+                            <i class="icon-footer ti-email"></i>
+                            <a href="">cskh@khavico.com</a>
+                        </li>
+                    </ul>
+
+            </div>
+            </li>
+
+            <li>
+                <div class="footer-box">
+                <h3>Hệ thống điểm bán</h3>
+                
+                    <ul>
+                        <li>
+                            Danh sách điểm bấn Bình định
+                        </li>
+                        <li>
+                            Danh sách điểm bán tại Đà Nẵng
+                        </li>
+                        <li>
+                            Danh sách điểm bán hàng tại HCM
+                        </li>
+                        <li>
+                             Danh sách điểm bán tại Nha Trang
+                        </li>
+                        <li>
+                            Các điểm bán khác trên toàn quốc
+                        </li>
+                    </ul>  
+                </div>           
+            </li>
+                 
+           
+            <li>
+                <div class="footer-box">
+                <h3>Liên kết nhanh</h3>
+                    <ul>
+                        <li>
+                            Tìm kiếm
+                        </li>
+                        <li>
+                            Giới thiệu
+                        </li>
+                        <li>
+                            Chính sách đổi trả
+                        </li>
+                        <li>
+                            Chính sách bảo mật
+                        </li>
+                        <li>
+                            Chính sách vận chuyển
+                        </li>
+                        <li>
+                            Điều khoản dịch vụ
+                        </li>
+                        <li>
+                            ĐC: 16 An Dương Vương, Nguyễn Văn Cừ, Quy Nhơn, Bình Định   
+                        </li>
                     </ul>
                 </div>
-                 
             </li>
+
+                
+
             <li>
-                <h3>Hệ thống điểm bán</h3>
-            </li>
-            <li>
-                <h3>Liên kết nhanh</h3>
-            </li>
-            <li>
-                <h3>Nhận tin mới</h3>
-            </li>
-            <li>
-                <h3>Hỗ trợ khách hàng</h3>
+                <div class="footer-box">
+                    <h3>Hỗ trợ khách hàng</h3>
+                </div>
+                
             </li>
         </ul>
     </div>
