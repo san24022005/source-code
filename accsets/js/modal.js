b9b641881d8b5328f4c0f2261029fbb2ed049eee
Siu Sab <san24022005@example.com>
2025-06-01 13:33:50 +0700

    sua lai file modal

--- accsets/js/modal.js
+++ accsets/js/modal.js
@@ -143,16 +143,3 @@ document.getElementById("btn-giohang").addEventListener("click", function () {
 
 });
 
-function updateRowspan() {
-  const td = document.getElementById("responsive-modal");
-  if (window.innerWidth <= 660) {
-    td.rowSpan = 1; // Mobile
-  } else {
-    td.rowSpan = 8; // PC
-  }
-}
-
-// Gọi hàm khi tải trang và khi thay đổi kích thước màn hình
-window.addEventListener("load", updateRowspan);
-window.addEventListener("resize", updateRowspan);
-
